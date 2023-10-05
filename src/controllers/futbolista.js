54ef170edae42425fc3049bc12e6eda19397eff8
marilin <puchetamarilin@gmail.com>
2023-10-05 00:49:26 -0300

    Cambios

--- src/controllers/futbolista.js
+++ src/controllers/futbolista.js
@@ -3,7 +3,7 @@ const futbolistaBD = require('../database/futbolistaBD')
 exports.buscarFutbolista = async(req, res) => {
     try{ 
         
-        const futbolistas = await futbolistaBD.buscarFutbolista();
+        const futbolistas = await futbolistaBD.buscarTodos();
 
         res.json({estado:'OK', dato:futbolistas});
         
@@ -13,7 +13,42 @@ exports.buscarFutbolista = async(req, res) => {
     }catch (exec){
         throw exec;
     }
-}
+};
+
+exports.buscarPorIdFutbolista = async(req, res) => {
+    try{
+        const idFutbolista = req.params.idFutbolista;   
+        
+        if(!idFutbolista) {
+            res.status(404).json({estado:'FALLO', msj:'Falta el id'});
+        }
+
+        const futbolista = await futbolistaBD.buscarPorId(idFutbolista);
+
+        res.json({estado:'OK', dato:futbolista});
+
+    }catch (exec){
+        throw exec;
+    }
+};
+
+
+exports.eliminarFutbolista = async (req, res) => {
+    const idFutbolista = req.params.idFutbolista;
+
+    if(!idFutbolista){
+        res.status(404).json({estado:'FALLO', msj:'no se especifico el id del Futbolista'});
+    }else{
+        try{
+            await futbolistaBD.eliminar(idFutbolista);
+            res.status(200).json({estado:'OK', msj:'Futbolista eliminado'});
+        }catch (error){
+            throw exec;
+        }
+    }
+};
+
+
 
 
 
--- src/database/futbolistaBD.js
+++ src/database/futbolistaBD.js
@@ -1,6 +1,6 @@
 const conexion = require('./conexionBD');
 
-exports.buscarFutbolista = async () => {
+const buscarTodos = async () => {
 
     const consulta = `SELECT  dni, nombre, apellido,
     (CASE
@@ -18,4 +18,32 @@ exports.buscarFutbolista = async () => {
     return futbolistas;
 }
 
+const buscarPorId = async (idFutbolista) => {
 
+    const consulta = `SELECT  dni, nombre, apellido,
+    (CASE
+        WHEN posicion = 0 THEN 'arquero'
+        WHEN posicion = 1 THEN 'defensor'
+        WHEN posicion = 2 THEN 'mediocampista'
+        WHEN posicion = 3 THEN 'delantero'
+        ELSE ''
+    END) AS posicion 
+    FROM futbolista 
+    WHERE activo = 1 AND idFutbolista = ?` ;
+
+    const [futbolista] = await conexion.query(consulta, idFutbolista);    
+
+    return futbolista;
+}
+
+const eliminar = async (idFutbolista) => {
+    const consulta = 'UPDATE futbolista SET activo = 0 WHERE idFutbolista = ?';
+    await conexion.query(consulta, [idFutbolista]);    
+}
+
+module.exports ={
+    buscarTodos,
+    eliminar,
+    buscarPorId
+
+}
