bfc120fdee115682fd3126d458af614e7547db55
marilin <puchetamarilin@gmail.com>
2023-10-04 21:16:32 -0300

    Index

--- index.js
+++ index.js
@@ -7,6 +7,15 @@ require('dotenv').config();
 
 const morgan = require('morgan');
 
+//para trabajar con el sistema de archivos: crear leer etc archivos
+
+const fs = require('fs');
+
+// trabajar con las rutas de archivos y directorios del sistema de archivos
+
+const path = require('path');
+
+  
 
 // utilizar el módulo cors
 const cors = require('cors');
@@ -21,14 +30,23 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
+// CREA UN ARCHIVO DE ACCESO
+// create a write stream (in append mode)
+// console.log(__dirname);
+var accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' })
+// setup the logger
+app.use(morgan('combined', { stream: accessLogStream }))
 
 // Rutas del API
 const v1Publico = require('./src/v1/routes/publico');
 const v1Futbolista = require('./src/v1/routes/futbolista');
+const v1Convocatoria = require('./src/v1/routes/convocatoria')
 
 // middleeare api (use las rutas definidas)
 app.use('/api/v1/publico', v1Publico);
 app.use('/api/v1/futbolista', v1Futbolista);
+app.use('/api/v1/convocatoria', v1Convocatoria);
+
 
 
 
